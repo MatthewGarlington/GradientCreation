c402ba3d2611a70341783ef3d30caf05e897298f
Matthew Garlington <61671107+MatthewGarlington@users.noreply.github.com>
2022-07-06 21:15:36 -0400

    Init commit

--- Sources/GradientCreation/GradientCreation.swift
+++ Sources/GradientCreation/GradientCreation.swift
@@ -1,6 +1,20 @@
-public struct GradientCreation {
-    public private(set) var text = "Hello, World!"
 
-    public init() {
+import SwiftUI
+
+public struct GradientView: View {
+    public init() { }
+    public var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Goodbeye, world!")
+        }
+    }
+}
+
+public struct GradientView_Previews: PreviewProvider {
+    public static var previews: some View {
+        GradientView()
     }
 }
